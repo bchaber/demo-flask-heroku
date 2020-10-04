d5edf8c50a8ad3a93f01430cf13e7e233239a22e
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-10-04 22:12:06 +0200

    Simplify the demo application

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-# app.py
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
-
-@app.route('/about')
-def about():
-    return "It is a demo web application"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)

6e2601eed3c57f24b66c5a6bd5a235e928ff8641
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-10-04 22:55:08 +0200

    Fill up the demo

--- app.py
+++ app.py
@@ -1,9 +1,47 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+supported_languages = ["pl", "cz", "es", "en-gb", "en"]
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    lang = request.accept_languages.best_match(supported_languages)
+    if lang == "pl":
+        msg = "Witaj"
+    elif lang == "cz":
+        msg = "Ahoj"
+    elif lang == "es":
+        msg = "Hola"
+    elif lang == "en-gb":
+        msg = "Delighted to meet you"
+    else:
+        msg = "Howdy"
+    
+    return f"<h1>{msg}.</h1>"
 
+@app.route('/secret')
+def secret():
+    return "No entry!", 403
+
+@app.route('/brew/<beverage>')
+def brew(beverage):
+    if beverage == "coffee":
+        return '''<pre>
+    I am a teapot
+       _...._
+     .'  _ _ `.
+    | ."` ^ `". _,
+    \_;`"---"`|//
+      |       ;/
+      \_     _/
+        `"""`
+        </pre>''', 418
+    if beverage == "beer":
+        return "Silly you..." 
+    if beverage == "vodka":
+        return "на здоро́вье"
+    if beverage == "tea":
+        return "Which one? Green, red, black..."
+    return "Huh?", 400
+    
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
